57dbceaf20a623a2916f23eaac2f85b75de940e5
eternal17 <sarmadkhatri15@gmail.com>
2021-10-12 21:18:03 +0100

    completing other conditions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,6 +22,7 @@ func main() {
 	// 	fmt.Println("Error, too many arguments")
 	// 	os.Exit(0)
 	// }
+
 	// reading first file
 
 	givenText, _ := os.ReadFile(args[0])
@@ -29,12 +31,28 @@ func main() {
 	words := strings.Split(string(givenText), " ")
 
 	// loop over each word in array, satisfy conditions
-
 	for i, word := range words {
+		// fl := string(words[i+1][0])
 		if word == "(up)" {
 			words[i-1] = strings.ToUpper(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+		} else if word == "(low)" {
+			words[i-1] = strings.ToLower(words[i-1])
+		} else if word == "(cap)" {
+			words[i-1] = strings.Title(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+		} else if word == "(hex)" {
+			words[i-1] = HextoInt(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+		} else if word == "(bin)" {
+			words[i-1] = BintoInt(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+		} else if word == "a" && (string(words[i+1][0]) == "a") {
+			words[i] = "an"
 		}
 	}
+
+	// removeElement(words)
 	fmt.Println(words)
 
 	// man := os.WriteFile(args[1], manipulate, 0644)
@@ -42,3 +60,41 @@ func main() {
 	// 	log.Fatal(man)
 	// }
 }
+
+// conv hex to int
+func HextoInt(hex string) string {
+	number, _ := strconv.ParseInt(hex, 16, 64)
+	return fmt.Sprint(number)
+}
+
+// conv binary to int
+
+func BintoInt(bin string) string {
+	number, _ := strconv.ParseInt(bin, 2, 64)
+	return fmt.Sprint(number)
+}
+
+// func checkVowel(a string) bool {
+// 	vowels := []string{"a", "e", "i", "o", "u", "h"}
+
+// 	for _, ch := range vowels {
+// 		if a == ch {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// func removeElement(a []string) []string {
+// 	keyWords := []string{"(bin)", "(hex)", "(up)"}
+// 	for i, word := range a {
+// 		for _, keyword := range keyWords {
+// 			if word == keyword {
+// 				a = append(a[:i], a[i+1:]...)
+// 			}
+// 		}
+// 	}
+// 	return a
+// }
+
+// fl == "a" || fl == "e" || fl == "i" || fl == "o" || fl == "u" || fl == "h"
