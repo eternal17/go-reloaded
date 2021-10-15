a9869efb9a64b7d9bdec0082c0f02e30cc20b8f7
eternal17 <sarmadkhatri15@gmail.com>
2021-10-15 18:03:05 +0100

    all tests passed

--- main.go
+++ main.go
@@ -114,18 +114,51 @@ func ChangeA(s []string) []string {
 
 func Punctuations(s []string) []string {
 	puncs := []string{",", ".", "!", "?", ":", ";"}
+	// punc in the middle of a string connecting to word after
+	for i, word := range s {
+		for _, punc := range puncs {
+			if string(word[0]) == punc && string(word[len(word)-1]) != punc {
+				s[i-1] += punc
+				s[i] = word[1:]
+			}
+		}
+	}
+
+	// punc at end of string
 	for i, word := range s {
 		for _, punc := range puncs {
 			if (string(word[0]) == punc) && (s[len(s)-1] == s[i]) {
 				s[i-1] += word
 				s = s[:len(s)-1]
-			} else if string(word[0]) == punc && string(word[len(word)-1]) != punc {
-				s[i-1] += punc
-				s[i] = word[1:]
-			} else if string(word[0]) == punc && string(word[len(word)-1]) == punc {
+			}
+		}
+	}
+
+	// punc in middle of string
+	for i, word := range s {
+		for _, punc := range puncs {
+			if string(word[0]) == punc && string(word[len(word)-1]) == punc && s[i] != s[len(s)-1] {
 				s[i-1] += word
 				s = append(s[:i], s[i+1:]...)
 			}
+		}
+	}
+
+	// for apostrophe
+	count := 0
+	for i, word := range s {
+		if word == "'" && count == 0 {
+			count += 1
+			s[i+1] = word + s[i+1]
+			s = append(s[:i], s[i+1:]...)
+		}
+
+	}
+	//  for second apostrophe
+	for i, word := range s {
+		if word == "'" {
+			s[i-1] = s[i-1] + word
+			s = append(s[:i], s[i+1:]...)
 		}
 	}
 	return s
